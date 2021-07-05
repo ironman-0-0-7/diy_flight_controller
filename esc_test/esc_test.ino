c069163f6e0758026ac2f32be7d4b57b5c680cea
ironman-0-0-7 <68261412+ironman-0-0-7@users.noreply.github.com>
2021-07-05 09:37:43 +0530

    code for testing esc Tx Rx and gyro

---
+++ esc_test/esc_test.ino
@@ -0,0 +1,25 @@
+
+void setup() {
+DDRD |= B11110000;
+Serial.begin(57600);
+
+delay(3000);}
+
+void loop() {
+
+
+for (int i=900 ; i<2000 ; i ++ )
+{
+  Serial.println(i);
+PORTD |= B11110000;
+delayMicroseconds(i);    
+PORTD &= B00001111;
+delay(100);
+}
+
+
+
+
+ 
+
+}
