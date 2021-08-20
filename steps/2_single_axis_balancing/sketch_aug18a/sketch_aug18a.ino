f9b8b5e57b1118774637828d95cebd8ac944349a
ironman-0-0-7 <68261412+ironman-0-0-7@users.noreply.github.com>
2021-08-20 10:53:12 +0530

    cal gyro offset . Gyro value conversion

--- steps/2_single_axis_balancing/sketch_aug18a/sketch_aug18a.ino
+++ steps/2_single_axis_balancing/sketch_aug18a/sketch_aug18a.ino
@@ -1,37 +1,69 @@
 #include <Wire.h>
-
 const int gyro_addr=0x68;
 int temp;
 int acc_reading[3];
 int gyro_reading[3];
+/*
+gyro[0] => pitch axis 
+gyro[1] => roll axis 
+gyro[2] => yaw axis 
+*/
 
 
-int cal_acc[3];
-int cal_gyro[3];
+
+int gyro_offset[3];
+
+int frequency_of_gyro_reading=250;
+
+
+
+int gyro_yaw,gyro_roll,gyro_pitch;
 
 
 
 
-
+//####################################################### SETUP #################################################################################
 void setup() {
-Serial.begin(57600);
-Serial.println("hlo world ");
-gyro_setup(gyro_addr);
-Serial.println("before cal acc_value");
-read_mpu(gyro_addr);
-Serial.println(acc_reading[2]);
-cal_mpu(gyro_addr,50);
-Serial.println("cal_value");
-Serial.println(cal_gyro[0]);
-Serial.println(cal_gyro[1]);
-Serial.println(cal_gyro[2]);
-Serial.println("acc_value");
-read_mpu(gyro_addr);
-Serial.println(gyro_reading[0]);
-Serial.println(gyro_reading[1]);
-Serial.println(gyro_reading[2]);
-}
 
+Serial.begin(57600);
+Serial.println("hello world ! ! !");
+Serial.println("setting up gyro");
+gyro_setup(gyro_addr);//Setting up gyro registers
+Serial.println("Calculating  gyro ofset");
+cal_mpu(gyro_addr,50);// Calculating gyro gyro_offset
+
+
+
+//Serial.println("before cal acc_value");
+//read_mpu(gyro_addr);
+//Serial.println(acc_reading[2]);
+
+
+
+//Serial.println("cal_value");
+//Serial.println(gyro_offset[0]);
+//Serial.println(gyro_offset[1]);
+//Serial.println(gyro_offset[2]);
+//Serial.println("acc_value");
+//read_mpu(gyro_addr);
+//Serial.println(gyro_reading[0]);
+//Serial.println(gyro_reading[1]);
+//Serial.println(gyro_reading[2]);
+
+
+
+
+//TODO  ********************** setting initial angle value test
+
+gyro_yaw=0;
+gyro_roll=0;
+gyro_pitch=0;
+
+
+
+
+}
+//####################################################################### LOOP  ############################################################
 void loop() {
   /*
 Serial.println("acc_value xx");
@@ -41,6 +73,9 @@ Serial.println(gyro_reading[2]);
 Serial.println(gyro_reading[0]);
 delay(300);
 */
+read_mpu(gyro_addr);
+Serial.println(gyro_pitch);
+delay(4);
 }
 
 
@@ -68,8 +103,6 @@ void gyro_setup(int gyro_addr)
     Wire.write(0x03);                                                          //Set the register bits as 00000011 (Set Digital Low Pass Filter to ~43Hz)
     Wire.endTransmission();                                                    //End the transmission with the gyro
 }
-
-
 //################################################################################### READ MPU ###########################################################################################################
 void read_mpu(int gyro_addr){
     //Read the MPU-6050
@@ -81,69 +114,81 @@ void read_mpu(int gyro_addr){
     acc_reading[0] = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the acc_x variable.
     acc_reading[1] = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the acc_y variable.
     acc_reading[2] = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the acc_z variable.
+    
     temp = Wire.read()<<8|Wire.read();                               //Add the low and high byte to the temperature variable.
-    gyro_reading[0] = Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
-    gyro_reading[1] = Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
+
+     
+    gyro_reading[1]= Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
+    gyro_reading[0]= Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
     gyro_reading[2] = Wire.read()<<8|Wire.read();                              //Read high and low part of the angular data.
 
 
-//SUbstrcting the initial value calculated during calibration
-acc_reading[0]=acc_reading[0]-cal_acc[0];
-acc_reading[1]=acc_reading[1]-cal_acc[1];
-acc_reading[2]=acc_reading[2]-cal_acc[2];
 
 
-gyro_reading[0]=gyro_reading[0]-cal_gyro[0];
-gyro_reading[1]=gyro_reading[1]-cal_gyro[1];
-gyro_reading[2]=gyro_reading[2]-cal_gyro[2];
+    
+
+//SUbstrcting the gyro_offset value calculated during calibration
+
+gyro_reading[0]=gyro_reading[0]-gyro_offset[0];
+gyro_reading[1]=gyro_reading[1]-gyro_offset[1];
+gyro_reading[2]=gyro_reading[2]-gyro_offset[2];
 
 
+//Converting and then intergrating gyro value
+//current value = previous value + converted gyro value * dt
+// degree per second  = raw gyro /65.5
+// frequency of gyro is frequency_of_gyro_reading
 
-
-
+//........................................................................
+int conversion_constant= frequency_of_gyro_reading*65.5 ;
+gyro_yaw=gyro_yaw+gyro_reading[2]/conversion_constant;
+gyro_roll=gyro_roll+gyro_reading[1]/conversion_constant;
+gyro_pitch=gyro_pitch+gyro_reading[0]/conversion_constant;
+//.......................................................................
 
 }
 
 //############################################################################# Calibrate MPU  ###################################################################################################################
-
 void cal_mpu(int gyro_addr,int num_of_rounds)
 {
-long acc_sum[3];
 long gyro_sum[3];
-Serial.println("Calibrating.");
-
-      acc_sum[0]=0;
-      acc_sum[1]=0;
-      acc_sum[2]=0;
-
+Serial.print("Calibrating gyro:");
       gyro_sum[0]=0;
       gyro_sum[1]=0;
       gyro_sum[2]=0;
 
 
+  gyro_offset[0]=0;
+  gyro_offset[1]=0;
+  gyro_offset[2]=0;
 
 
     for (int i=0;i<num_of_rounds;i++)
     {
+      
       Serial.print(".");
       read_mpu(gyro_addr);
-      acc_sum[0]=acc_sum[0]+acc_reading[0];
-      acc_sum[1]=acc_sum[1]+acc_reading[1];
-      acc_sum[2]=acc_sum[2]+acc_reading[2];
+
 
       gyro_sum[0]=gyro_sum[0]+gyro_reading[0];
       gyro_sum[1]=gyro_sum[1]+gyro_reading[1];
       gyro_sum[2]=gyro_sum[2]+gyro_reading[2];
       delay(100);
     }
+    
     Serial.println("Done ! ");
-  cal_acc[0]=acc_sum[0]/num_of_rounds;
-  cal_acc[1]=acc_sum[1]/num_of_rounds;
-  cal_acc[2]=acc_sum[2]/num_of_rounds;
+  gyro_offset[0]=gyro_sum[0]/num_of_rounds;
+  gyro_offset[1]=gyro_sum[1]/num_of_rounds;
+  gyro_offset[2]=gyro_sum[2]/num_of_rounds;
 
-  cal_gyro[0]=gyro_sum[0]/num_of_rounds;
-  cal_gyro[1]=gyro_sum[1]/num_of_rounds;
-  cal_gyro[2]=gyro_sum[2]/num_of_rounds;
+
+Serial.print("gyro ofset 0: ");
+Serial.print(gyro_offset[0]);
+Serial.print("|| gyro ofset 1: ");
+Serial.print(gyro_offset[1]);
+Serial.print("|| gyro ofset 2: ");
+Serial.print(gyro_offset[2]);
+
 
 }
 //#######################################################################################################################################################################
